873e27b026e147bc77bd1990bb2e7643146cd5fa
Taha Maqsood <154565199+TahaMaqsood23@users.noreply.github.com>
2024-01-17 14:56:14 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+import inquirer from "inquirer";
+let randomNumber = Math.floor(Math.random() * 10);
+for (let i = 0; i < 3; i++) {
+    let guess = await inquirer.prompt({
+        type: 'input',
+        name: 'userguess',
+        message: 'Guess the random number'
+    });
+    if (guess.userguess === randomNumber) {
+        console.log('Correct guess');
+    }
+    else {
+        console.log('wrong guess');
+    }
+}
